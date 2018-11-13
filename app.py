b8cf9f8e0948c1260e0b7331d767fbf3d12cc576
Amy Wright <aimbit@Amys-MacBook-Pro.local>
2018-11-12 19:28:52 -0800

    added static and templates

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	 return 'Hello, Flask!'
+	 return render_template("index.html")
 
+@app.route("/page")
+def page1():
+	return "Hi Amy, Finish your lab"
+		
 if __name__ == '__main__':
 	 app.run(debug=True)
