a6757d306ce26aa3856af4e7f7817b2fe97bb284
Amy Wright <aimbit@Amys-MacBook-Pro.local>
2018-11-18 21:01:14 -0800

    added error page

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
-from flask import Flask, render_template,redirect, url_for, request, session, redirect, escape
+from flask import Flask, render_template,redirect, url_for, request, session, redirect, escape, abort
 app = Flask(__name__)
 app.secret_key = 'amy'
 
+usernameKey = 'username'
+passwordKey = 'password'
+
 @app.route('/')
 def index():
-	if 'username' in session:
-		u = session['username']
-		return render_template("index.html", uname=u)
-	return render_template("index.html", uname='waht')
+	return render_template("index.html")
 
 
 @app.route("/page")
 def page1():
-	return "Hi Amy, Finish your lab"
+	if usernameKey in session:
+		return 'the good stuff' + usernameKey
+	return abort(418)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
 	error = None
 	if request.method == 'POST':
-		if str(request.form['username']) != 'admin' or str(request.form['password']) != "password":
+		if str(request.form[usernameKey]) != 'admin' or str(request.form[passwordKey]) != "password":
 			error = 'Wrong username or password.'
 		else:
-			session['usermame'] = request.form['username']
-			return redirect(url_for('index'))
+			session[usernameKey] = request.form[usernameKey]
+			return redirect(url_for('page1'))
 	return render_template('login.html', error=error)
 
 @app.route('/logout')
 def logout():
-   session.pop('username', None)
+   session.pop(usernameKey, None)
    return redirect(url_for('index'))
+
+@app.errorhandler(418)
+def imATeapot(e):
+    return render_template('418.html'), 418
 
 if __name__ == '__main__':
 	 app.run(debug=True)
